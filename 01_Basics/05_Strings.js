42fdd5ed181594f9e11494b87f444dd1f95d4f47
Himanshu Mehra <150444857+Himanshuu-1@users.noreply.github.com>
2025-07-06 16:49:44 +0000

    StringsLect

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,19 @@
+const name="Himanshu";
+const age=20;
+
+// console.log(`Hello my name is ${name} and age is ${age}`)
+// console.log(name[0]);
+
+// upper case
+// const uppercase=name.toUpperCase();
+
+// Fixed Length for fixing points value
+// console.log(age.toFixed(3));
+
+//To COnvert the integer to a String 
+// console.log(age.toString())
+
+//For reading the digit more easily use .localeString
+const num=100000;
+console.log(num.toLocaleString('en-IN'))
+// console.log(age.toString().length)
